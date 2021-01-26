0ffee06c06f3ffadaf6c8902a8ca2589931e9577
nikola.micic <nikola.micic@ikor.de>
2021-01-26 21:05:48 +0100

    RSocket Fire-And-Forget, Stream and Channel types

--- rsocket-spring-blog/rsocket-spring-boot-client/src/main/kotlin/io/mickeckemi21/rsocketspringbootclient/shell/RSocketShellComponent.kt
+++ rsocket-spring-blog/rsocket-spring-boot-client/src/main/kotlin/io/mickeckemi21/rsocketspringbootclient/shell/RSocketShellComponent.kt
@@ -7,6 +7,11 @@ import org.springframework.messaging.rsocket.RSocketRequester
 import org.springframework.messaging.rsocket.connectTcpAndAwait
 import org.springframework.shell.standard.ShellComponent
 import org.springframework.shell.standard.ShellMethod
+import reactor.core.Disposable
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import java.time.Duration
+import java.time.Duration.ofSeconds
 
 @ShellComponent
 class RSocketShellComponent(private val rSocketRequesterBuilder: RSocketRequester.Builder) {
@@ -20,15 +25,59 @@ class RSocketShellComponent(private val rSocketRequesterBuilder: RSocketRequeste
             .connectTcpAndAwait("localhost", 7000)
     }
 
+    private lateinit var disposable: Disposable
+
     @ShellMethod("Send one request, one response will be printed")
     fun requestResponse() {
-        log.info("\nSending one request. Waiting for one response...")
+        log.info("\nRequest-Response. Sending one request. Waiting for one response...")
         val response = rSocketRequester
             .route("request.response")
-            .data(Message("Client", "Request"))
+            .data(Message("CLIENT", "REQUEST"))
             .retrieveMono(Message::class.java)
             .block()
         log.info("\nResponse was: {}", response)
+    }
+
+    @ShellMethod("Send one request. No response will be returned")
+    fun fireAndForget() {
+        log.info("\nFire-and-Forget. Sending one request. Expect no response (check server logs)...")
+        rSocketRequester
+            .route("fire.and.forget")
+            .data(Message("CLIENT", "FIRE_AND_FORGET"))
+            .send()
+            .block()
+    }
+
+    @ShellMethod("Send one request. Many responses (stream) will be printed")
+    fun stream() {
+        log.info("\nRequest-Stream. Sending one request. Waiting for unlimited responses (Stop process to quit)...");
+        disposable = rSocketRequester
+            .route("stream")
+            .data(Message("CLIENT", "STREAM"))
+            .retrieveFlux(Message::class.java)
+            .subscribe { res -> log.info("Response received: {}", res) }
+    }
+
+    @ShellMethod("Stream some settings to the server. Stream of responses will be printed")
+    fun channel() {
+        val setting1 = Mono.just(ofSeconds(1L))
+        val setting2 = Mono.just(ofSeconds(3L)).delayElement(ofSeconds(5L))
+        val setting3 = Mono.just(ofSeconds(5L)).delayElement(ofSeconds(15L))
+        val settings = Flux.concat(setting1, setting2, setting3)
+            .doOnNext { log.info("\nSending setting for ${it.seconds}-second interval.\n") }
+
+        disposable = rSocketRequester
+            .route("channel")
+            .data(settings)
+            .retrieveFlux(Message::class.java)
+            .subscribe { msg -> log.info("Received: $msg \n(Type 's' to stop.)") }
+    }
+
+    @ShellMethod("Stop streaming messages from he server")
+    fun s() {
+        if (this::disposable.isInitialized) {
+            disposable.dispose()
+        }
     }
 
 }
--- rsocket-spring-blog/rsocket-spring-boot-server/src/main/kotlin/io/mickeckemi21/rsocketspringbootserver/controller/RSocketController.kt
+++ rsocket-spring-blog/rsocket-spring-boot-server/src/main/kotlin/io/mickeckemi21/rsocketspringbootserver/controller/RSocketController.kt
@@ -4,6 +4,8 @@ import io.mickeckemi21.rsocketspringbootserver.model.Message
 import org.slf4j.LoggerFactory
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.stereotype.Controller
+import reactor.core.publisher.Flux
+import java.time.Duration
 
 @Controller
 class RSocketController {
@@ -18,6 +20,33 @@ class RSocketController {
         // create a single Message and return it
         return Message("SERVER", "RESPONSE")
     }
+
+    @MessageMapping("fire.and.forget")
+    fun fireAndForget(request: Message) {
+        log.info("Received fire-and-forget request: {}", request)
+    }
+
+    @MessageMapping("stream")
+    fun stream(request: Message): Flux<Message> {
+        log.info("Received stream request: {}", request)
+        return Flux.interval(Duration.ofSeconds(1L))
+            .map { Message("SERVER", "STREAM", it) }
+            .log()
+    }
+
+    @MessageMapping("channel")
+    fun channel(settings: Flux<Duration>): Flux<Message> {
+        log.info("Received channel request")
+        return settings
+            .doOnNext { setting ->
+                log.info("\nFrequency setting is ${setting.seconds} second(s)\n")
+            }.switchMap { setting ->
+                Flux.interval(setting).map {
+                    Message("SERVER", "CHANNEL", it)
+                }
+            }.log()
+    }
+
 
 }
 
